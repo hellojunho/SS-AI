980f72ca449ba2c50c75d62f7d4c7ca033576d64
hellojunho-molcube <junho@molcube.com>
2026-02-13 14:19:30 +0900

    Reconfigure Redis and backend ports, and add `.env` files to gitignore.

--- backend-drf/entrypoint.sh
+++ backend-drf/entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 python manage.py migrate --run-syncdb --noinput
 python manage.py bootstrap_ss_ai
-exec python manage.py runserver 0.0.0.0:8000
+exec python manage.py runserver 0.0.0.0:8080
